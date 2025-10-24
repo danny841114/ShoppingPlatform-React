0a082c6506430893b9c2e9156a383acd31028772
danny <danny841114@gmail.com>
2025-10-24 21:55:07 +0800

    重購部分代碼

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState, Fragment } from "react";
-import { API_BASE_URL } from "../util/config";
+import { useEffect, useState } from "react";
 import { getProducts } from "../util/api";
+import { getProductImageUrl } from "../util/api";
 
 export default function Index() {
   const [products, setProducts] = useState([]); // 陣列解構賦值（array destructuring）
@@ -16,55 +16,52 @@ export default function Index() {
   }, []);
 
   return (
-    <Fragment>
-      <div className="container mt-5">
-        <h3>商品列表</h3>
+    <div className="container mt-5">
+      <h3>商品列表</h3>
 
-        <div className="mt-2" v-if="totalElements">
-          總共有 {products.length} 個商品
-        </div>
+      <div className="mt-2">總共有 {products.length} 個商品</div>
 
-        <div className="mt-2">
-          <form>
-            <label htmlFor="sizeSelect">每頁顯示&emsp;</label>
-            <select id="sizeSelect">
-              <option value="4">4</option>
-              <option value="8">8</option>
-              <option value="12">12</option>
-            </select>
-            <span>&emsp;個商品</span>
-          </form>
-        </div>
+      <div className="mt-2">
+        <form>
+          <label htmlFor="sizeSelect">每頁顯示&emsp;</label>
+          <select id="sizeSelect">
+            <option value="4">4</option>
+            <option value="8">8</option>
+            <option value="12">12</option>
+          </select>
+          <span>&emsp;個商品</span>
+        </form>
+      </div>
 
-        <div className="mt-2">
-          <button className="btn btn-primary">上一頁</button>
+      <div className="mt-2">
+        <button className="btn btn-primary">上一頁</button>
 
-          <span>&emsp;第 N 頁&emsp;</span>
+        <span>&emsp;第 N 頁&emsp;</span>
 
-          <button className="btn btn-primary">下一頁</button>
+        <button className="btn btn-primary">下一頁</button>
 
-          <span>&emsp;共 N 頁&emsp;</span>
-        </div>
+        <span>&emsp;共 N 頁&emsp;</span>
+      </div>
 
-        <div className="row mt-3">
-          {products.map((p) => (
-            <div key={p.id} className="col-md-3 mb-4">
-              <div className="card" style={{ width: 288 }}>
-                <img
-                  src={`${API_BASE_URL}/api/product/${p.id}/photo`}
-                  className="card-img-top p-4"
-                  alt=""
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{p.name}</h5>
-                  <p className="card-text">{p.description}</p>
-                  <p className="card-text text-muted">賣家：{p.member?.id}</p>
-                </div>
+      <div className="row mt-3">
+        {products.map((p) => (
+          <div key={p.id} className="col-md-3 mb-4">
+            <div className="card" style={{ width: 288 }}>
+              <img
+                // src={`${API_BASE_URL}/api/product/${p.id}/photo`}
+                src={getProductImageUrl(p.id)}
+                className="card-img-top p-4"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title">{p.name}</h5>
+                <p className="card-text">{p.description}</p>
+                <p className="card-text text-muted">賣家：{p.member?.id}</p>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    </Fragment>
+    </div>
   );
 }
--- src/util/api.js
+++ src/util/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { API_BASE_URL } from "../util/config";
 
+// 回傳 Promise
 const api = axios.create({
-  baseURL: `${API_BASE_URL}`,
+  baseURL: API_BASE_URL,
 });
 
+export const getProductImageUrl = (id) => `${API_BASE_URL}/api/product/${id}/photo`; // 回傳 URL 字串
 export const getProducts = () => api.get(`/api/product`);
